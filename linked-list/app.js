1b5ce76c51fca81892467aa97a12262572de0670
igorcervac <igor.cervac@outlook.com>
2025-08-25 12:15:21 -0400

    Implement linked list

---
+++ linked-list/app.js
@@ -0,0 +1,14 @@
+import LinkedList from "./linked-list.js";
+
+let linkedList = new LinkedList();
+
+console.group('Append:');
+linkedList.append(1);
+linkedList.append(2);
+linkedList.print();
+console.groupEnd();
+
+console.group('Remove:');
+linkedList.remove(2);
+linkedList.remove(1);
+console.groupEnd();
---
+++ linked-list/linked-list.js
@@ -0,0 +1,54 @@
+class Node {    
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+export default class LinkedList {    
+    constructor() {
+        this.head = null;
+    }
+
+    append(value){
+        if (!this.head){
+            this.head = new Node(value);
+        } else {
+            let node = new Node(value);
+
+            let current = this.head;
+            while(current.next){
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+    }
+
+    remove(value){
+        let current = this.head;
+        let prev = null;
+
+        if (this.head.value == value){
+            this.head = this.head.next;
+            return;
+        }
+
+        while(current && current.value != value){
+            prev = current;
+            current = current.next;           
+        }
+
+        if (current){
+            prev.next = current.next;
+        }
+    }
+
+    print(){
+        let current = this.head;
+        while(current != null){
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
