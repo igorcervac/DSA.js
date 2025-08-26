aec1cbb63cd2291ccbe3d43a49447971a3ad8a1a
igorcervac <igor.cervac@outlook.com>
2025-08-26 13:33:33 -0400

    Implement binary tree

---
+++ binary-tree/app.js
@@ -0,0 +1,21 @@
+import BinaryTree from "./binary-tree.js";
+
+const tree = new BinaryTree();
+tree.insert(9);
+tree.insert(7);
+tree.insert(11);
+tree.insert(3);
+tree.insert(5);
+
+console.group('In-order:');
+tree.traverseInOrder();
+console.groupEnd();
+
+console.group('Pre-order:');
+tree.traversePreOrder();
+console.groupEnd();
+
+console.group('Post-order:');
+tree.traversePostOrder();
+console.groupEnd();
+
